79973ad49a2cd54245ad89d68b7c68a95a74b51d
fanty <524977004@qq.com>
2017-01-14 20:18:54 +0800

    charpt 4 flash 4c

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
@@ -34,8 +34,12 @@ def internal_server_error(e):
 def index():
     form = NameForm()
     if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('looks like you have changed your name!')
         session['name'] = form.name.data
         return redirect(url_for('index'))
+    print session
     return render_template('index.html', form=form, name=session.get('name'))
 
 
